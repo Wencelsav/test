1d3e410da908960b433856c005279fba6f0d103f
Wencelsav <31541176+Wencelsav@users.noreply.github.com>
2017-10-13 19:45:02 +0800

    Update 003.cpp

--- 003.cpp
+++ 003.cpp
@@ -22,22 +22,32 @@ int main() {
 
 		for (int i = 0; i < 10; ++i) {
 			int j = ++i;
-				if((s[j] < s[i])||	(x[j]<x[i])) {
+				if(s[j] < s[i]) {
 				cout << "smb do not let to be glad" << endl;
 			}
-
-
-		for ( int i=0; i<20; i++) {
-		    if(s[i]<x[i])
-		    {z[i/2]=s[i];
-		    z[i]=x[i];}
-		    
-    
-    }
-    }
+		}
 
 	}
+	for (string string; getline(cin, string); ) {
+
+		istringstream stream(string);
+
+	
+
+		for (int i = 0; i < 10; ++i) {
+			int j = ++i;
+				if(x[j]<x[i]) {
+				cout << "smb do not let to be glad" << endl;
+			}
+		}
+	}
+    for (int i=0; i<10; ++i){
+        
+        if(s[i]<x[i])
+        {z[i/2]=s[i];
+        z[i]=x[i];
+        }
 	cout << z<< endl;
 	return 0;
-	
+    }
 	}
