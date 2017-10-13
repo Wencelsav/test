4dbe0bedc96ea18c5f8a361ea2dfbb0126fac5e9
Wencelsav <31541176+Wencelsav@users.noreply.github.com>
2017-10-13 10:46:03 +0300

    Create 003.cpp

---
+++ 003.cpp
@@ -0,0 +1,57 @@
+#include "stdafx.h"
+#include <iostream>
+
+#include "sstream"
+
+
+
+using namespace std;
+
+
+
+int main() {
+	int  s[n],n;
+
+
+
+	for (string string; getline(cin, string); ) {
+
+		istringstream stream(string);
+
+		bool failure = false;
+
+		for (int i = 0; i < 10; ++i) {
+			int j = ++i
+				If(s[j] < s[i]) {
+				cout << 'error' << endl;
+			}
+
+
+		}
+
+
+
+	}
+	return;
+	
+	}
+void exp(char* str)
+{
+	int i = 0;
+	int j = 0;
+	int a, b, c, d;
+		c=a+b;
+
+	for (string string; getline(cin, string); ) {
+
+		istringstream stream(string);
+		while ((*(str + i) = *(str + j++)) != 'b') {}
+
+		while ((*(str + i) = *(str + j++)) != 'a') {}
+		
+		if (d == c) {
+			cout << a, b << endl;
+		}
+		else cout << error << endl;
+	}
+}
